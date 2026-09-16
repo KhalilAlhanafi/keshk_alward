9e879bd764c06eff5757c953974169db6bacc23f
KhalilAlhanafi <smartkhalilalhanafi@gmail.com>
2026-09-16 14:00:39 +0300

    doneeeeessssstttttaaaaa

--- database/migrations/2026_09_16_130553_change_image_path_to_longtext_in_categories_and_settings_table.php
+++ database/migrations/2026_09_16_130553_change_image_path_to_longtext_in_categories_and_settings_table.php
@@ -14,13 +14,6 @@ return new class extends Migration
         Schema::table('categories', function (Blueprint $table) {
             $table->longText('image_path')->nullable()->change();
         });
-
-        Schema::table('settings', function (Blueprint $table) {
-            $table->longText('logo_path')->nullable()->change();
-            $table->longText('favicon_path')->nullable()->change();
-            $table->longText('qr_image_path')->nullable()->change();
-            $table->longText('hero_image_path')->nullable()->change();
-        });
     }
 
     /**
@@ -30,13 +23,6 @@ return new class extends Migration
     {
         Schema::table('categories', function (Blueprint $table) {
             $table->string('image_path', 255)->nullable()->change();
-        });
-
-        Schema::table('settings', function (Blueprint $table) {
-            $table->string('logo_path', 255)->nullable()->change();
-            $table->string('favicon_path', 255)->nullable()->change();
-            $table->string('qr_image_path', 255)->nullable()->change();
-            $table->string('hero_image_path', 255)->nullable()->change();
         });
     }
 };
--- tests/Pest.php
+++ tests/Pest.php
@@ -48,5 +48,3 @@ function something()
 {
     // ..
 }
-
-//kkkkkk
