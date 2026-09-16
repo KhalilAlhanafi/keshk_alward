b5daca7893580f8428b3f6ca27e76992ea34042d
KhalilAlhanafi <smartkhalilalhanafi@gmail.com>
2026-09-16 13:29:47 +0300

    doneeeeessssstttttaaa

--- database/migrations/2026_09_16_130553_change_image_path_to_longtext_in_categories_and_settings_table.php
+++ database/migrations/2026_09_16_130553_change_image_path_to_longtext_in_categories_and_settings_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 return new class extends Migration
 {
@@ -12,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         DB::statement('ALTER TABLE categories MODIFY image_path LONGTEXT');
-        
+
         DB::statement('ALTER TABLE settings MODIFY logo_path LONGTEXT');
         DB::statement('ALTER TABLE settings MODIFY favicon_path LONGTEXT');
         DB::statement('ALTER TABLE settings MODIFY qr_image_path LONGTEXT');
@@ -25,11 +27,10 @@ return new class extends Migration
     public function down(): void
     {
         DB::statement('ALTER TABLE categories MODIFY image_path VARCHAR(255)');
-        
+
         DB::statement('ALTER TABLE settings MODIFY logo_path VARCHAR(255)');
         DB::statement('ALTER TABLE settings MODIFY favicon_path VARCHAR(255)');
         DB::statement('ALTER TABLE settings MODIFY qr_image_path VARCHAR(255)');
         DB::statement('ALTER TABLE settings MODIFY hero_image_path VARCHAR(255)');
     }
 };
-
